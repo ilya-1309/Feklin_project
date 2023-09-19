71bd9a7be6485e69aa086e965b43d9f15464f7b4
Ilya <fekl13in@gmail.com>
2023-09-19 22:45:12 +0300

    Initial Commit

---
+++ Feklin_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Feklin_project
+//
+//  Created by Ilya Feklin on 19.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
